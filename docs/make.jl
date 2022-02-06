8c4e3564e213f49e404dc7e2efee9dc5ed706e16
Michael Goerz <goerz@stanford.edu>
2022-02-06 17:30:45 -0500

    Build documentation artifact in CI

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,7 @@
 using QuantumControlBase
 using QuantumPropagators
 using Documenter
+using Pkg
 
 DocMeta.setdocmeta!(
     QuantumControlBase,
@@ -9,14 +10,23 @@ DocMeta.setdocmeta!(
     recursive=true
 )
 
+PROJECT_TOML = Pkg.TOML.parsefile(joinpath(@__DIR__, "..", "Project.toml"))
+VERSION = PROJECT_TOML["version"]
+NAME = PROJECT_TOML["name"]
+AUTHORS = join(PROJECT_TOML["authors"], ", ") * " and contributors"
+GITHUB = "https://github.com/JuliaQuantumControl/QuantumControlBase.jl"
+
+println("Starting makedocs")
+
 makedocs(;
-    modules=[QuantumControlBase],
-    authors="Michael Goerz <mail@michaelgoerz.net>, Alastair Marshall <alastair@nvision-imaging.com>, and contributors",
+    authors=AUTHORS,
     sitename="QuantumControlBase.jl",
+    modules=[QuantumControlBase],
     format=Documenter.HTML(;
         prettyurls=true,
         canonical="https://juliaquantumcontrol.github.io/QuantumControlBase.jl",
-        assets=String[]
+        assets=String[],
+        footer="[$NAME.jl]($GITHUB) v$VERSION docs powered by [Documenter.jl](https://github.com/JuliaDocs/Documenter.jl)."
     ),
     pages=[
         "Home" => "index.md",
@@ -24,5 +34,7 @@ makedocs(;
         "History" => "history.md",
     ]
 )
+
+println("Finished makedocs")
 
 deploydocs(; repo="github.com/JuliaQuantumControl/QuantumControlBase.jl")
