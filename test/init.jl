9ba8958b724f2ba17d5fd2b886cbb7a876dab68c
Michael Goerz <goerz@stanford.edu>
2022-02-12 00:44:18 -0500

    Serve documentation with LiveServer

--- test/init.jl
+++ test/init.jl
@@ -1,26 +1,19 @@
 # init file for "make devrepl"
-using Pkg
-Pkg.develop(PackageSpec(path=pwd()))
 using Revise
 using JuliaFormatter
+using LiveServer: serve, servedocs
+
 println("""
 *******************************************************************************
 DEVELOPMENT REPL
 
-Revise is active. JuliaFormatter is active.
+Revise, JuliaFormatter, LiveServer are active.
 
-Run
-
-    include("test/runtests.jl")
-
-for running the entire test suite. Alternatively, run e.g.
-
-    include("test/test_propagate.jl")
-
-for running an individual test file. Run
-
-    include("docs/make.jl")
-
-to generate the documentation
+* `include("test/runtests.jl")` – Run the entire test suite
+* `include("docs/make.jl")` – Generate the documentation
+* `format(".")` – Apply code formatting to all files
+* `servedocs([port=8000, verbose=false])` –
+  Build and serve the documentation. Automatically recompile and redisplay on
+  changes
 *******************************************************************************
 """)
