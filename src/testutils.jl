4f0486b3d65b0181e7bd3882a7a53ad9b899f259
Michael Goerz <goerz@stanford.edu>
2022-10-29 03:20:10 -0400

    Add QuantumTestLogger

--- src/testutils.jl
+++ src/testutils.jl
@@ -12,6 +12,7 @@ export random_complex_matrix,
 export dummy_control_problem
 export test
 export show_coverage, generate_coverage_html
+export QuantumTestLogger
 
 using Logging
 using Random
@@ -647,6 +648,51 @@ function optimize_with_dummy_method(problem)
         wrk.result.optimized_controls[l] = discretize(ϵ⁽ⁱ⁾[l], problem.tlist)
     end
     return wrk.result
+end
+
+
+"""Logger that stores all messages in a `messages` field (array of strings).
+
+The `in` operator can be used to check whether a string or regex is in any of
+the recorded messages.
+
+```julia
+test_logger = QuantumTestLogger()
+with_logger(test_logger) do
+    #...
+end
+@test "message" in logger
+```
+
+This is a simplified version of `Logging.TestLogger`.
+"""
+struct QuantumTestLogger <: AbstractLogger
+    messages::Vector{String}
+    function QuantumTestLogger()
+        new(String[])
+    end
+end
+
+Logging.shouldlog(logger::QuantumTestLogger, level, _module, group, id) = true
+
+Logging.min_enabled_level(logger::QuantumTestLogger) = Logging.Debug
+
+Logging.catch_exceptions(logger::QuantumTestLogger) = false
+
+function Logging.handle_message(
+    logger::QuantumTestLogger,
+    level::Logging.LogLevel,
+    message,
+    args...;
+    kwargs...
+)
+    levelstr = string(level)
+    push!(logger.messages, "$levelstr: $message")
+    nothing
+end
+
+function Base.in(s, logger::QuantumTestLogger)
+    return any([occursin(s, msg) for msg in logger.messages])
 end
 
 
